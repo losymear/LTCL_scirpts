7ba8908fb8b9437db2b22cda94e66f0e5431d67f
benymor <w15868821768@hotmail.com>
2018-04-18 13:53:27 +0800

    chapter33

---
+++ Chapter32_Position_params/getopts2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ 
+while getopts ":fD:ea:q" opt; do
+echo $opt
+echo $OPTIND
+  case $opt in
+    a)
+      echo "-a was triggered, Parameter: $OPTARG" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
